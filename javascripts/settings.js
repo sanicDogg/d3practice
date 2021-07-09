f212615ad94756d0296d881e239f88cfab2092b7
sanicDogg <>
2021-07-09 22:43:30 +0300

    Страница настроек теперь работает, есть 3 критерия поиска

--- javascripts/settings.js
+++ javascripts/settings.js
@@ -1,10 +1,18 @@
+let selects = document.querySelectorAll("select");
+if (localStorage.length > 0)
+    selects.forEach(s => {
+        s.value = localStorage.getItem(s.name)
+    })
+
 document.querySelector(".indexRef").addEventListener("click", () => {
+    localStorage.clear();
+
     let form = document.querySelector("form");
-
     let formData = new FormData(form);
-    let studentsValue = formData.get("students")
-    // localStorage.setItem("students", studentsValue);
-    // console.log(formData.get("students"))
 
-    // window.location = "index.html";
+    localStorage.setItem("students", formData.get("students"));
+    localStorage.setItem("lessonType", formData.get("lessonType"));
+    localStorage.setItem("topicNum", formData.get("topicNum"));
+
+    window.location = "index.html";
 })
--- javascripts/utils.js
+++ javascripts/utils.js
@@ -29,3 +29,87 @@ export function getTooltipText (data) {
         + "<br>Количество часов: " + hoursCount
         + "<br>Место проведения: " + place + borderControlReady)
 }
+
+export function prepareData(data) {
+    // Если нужно отобразить график для одного студента
+    let student = getStudentLastName(Number(localStorage.getItem("students")));
+
+    // Список студентов
+    let students = Array.from(new Set(data.map(item => item.lastName)))
+
+    if (student) {
+        students = [student];
+    }
+
+    // Подготовка данных
+    let dataReady = students.map(student => {
+        return {
+            name: student,
+            values: data.filter((d) =>
+                getFilterExpression(d, student)
+            )
+        }
+    });
+
+    return {students: students, dataReady: dataReady}
+}
+
+function getStudentLastName(index) {
+    switch (index) {
+        case 0:
+            return false
+        case 1:
+            return "Табакин"
+        case 2:
+            return "Иванов"
+        case 3:
+            return "Петров"
+        case 4:
+            return "Сидоров"
+    }
+}
+
+function getLessonType(index) {
+    switch (index) {
+        case 0:
+            return false
+        case 1:
+            return "Л"
+        case 2:
+            return "ПЗ"
+        case 3:
+            return "ГЗ"
+        case 4:
+            return "КЗ"
+    }
+}
+
+function getTopicNum(index) {
+    switch (index) {
+        case 0:
+            return false
+        case 1:
+            return "В"
+        case 2:
+            return "1"
+        case 3:
+            return "2"
+        case 4:
+            return "3"
+        case 5:
+            return "4"
+    }
+}
+
+function getFilterExpression(d, student) {
+    let lessonType = getLessonType(Number(localStorage.getItem("lessonType")));
+
+    let lessonTypeExpression = d.lessonType === lessonType;
+    if (!lessonType) lessonTypeExpression = true;
+
+    let topicNum = getTopicNum(Number(localStorage.getItem("topicNum")));
+    let topicNumExpression = d.topicNum === topicNum;
+    if (!topicNum) topicNumExpression = true;
+
+    return (d.lastName === student) && lessonTypeExpression && topicNumExpression;
+}
